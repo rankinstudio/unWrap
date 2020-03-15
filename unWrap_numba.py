88eb6481a4380bd1e71391c59ab5788cd8df3d39
David Rankin <david@rankinstudio.com>
2020-03-14 19:26:48 -0700

    Add files via upload
    This is an updated version that uses GPU processing with the numba module for use on a PC. Overall speed is insanely faster.

---
+++ unWrap_numba.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+from numba import jit
+from timeit import default_timer as timer
+
+def unwrap(img, Cb):
+
+    #MAPPING
+    @jit(nopython=True)
+    def buildMap(Wd, Hd, R, Cx, Cy):
+        map_x = np.zeros((Hd, Wd), np.float32)
+        map_y = np.zeros((Hd, Wd), np.float32)
+
+        for y in range(0, int(Hd - 1)):
+            for x in range(0, int(Wd - 1)):
+                r = (float(y) / float(Hd)) * R
+                theta = (float(x) / float(Wd)) * 2.0 * np.pi
+                xS = Cx + r * np.sin(theta)
+                yS = Cy + r * np.cos(theta)
+                map_x[y, x] = int(xS)
+                map_y[y, x] = int(yS)
+        return map_x, map_y
+
+    #UNWARP QUICK
+    def unwarp(img, xmap, ymap):
+        output = cv2.remap(img, xmap, ymap, cv2.INTER_LINEAR)
+        return output
+
+    #IMAGE CENTER
+    Cx = img.shape[0]/2
+    Cy = img.shape[1]/2
+
+    #RADIUS OUTER
+    R = Cb - Cx
+
+    #DESTINATION IMAGE SIZE
+    Wd = int(abs(2.0 * (R / 2) * np.pi))
+    Hd = int(abs(R))
+
+    #BUILD MAP
+    xmap, ymap = buildMap(Wd, Hd, R, Cx, Cy)
+
+    #UNWARP
+    result = unwarp(img, xmap, ymap)
+
+    return result
+
